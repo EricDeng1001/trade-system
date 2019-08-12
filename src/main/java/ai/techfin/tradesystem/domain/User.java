70eb7f54c7e769ffc7423dc85c52624cbe2db03f
GitAntinus <gantinus@gmail.com>
2019-08-12 14:22:40 +0800

    增加权限控制
    Signed-off-by: GitAntinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/domain/User.java
+++ src/main/java/ai/techfin/tradesystem/domain/User.java
@@ -89,8 +89,8 @@ public class User extends AbstractAuditingEntity implements Serializable {
     @ManyToMany
     @JoinTable(
         name = "user_authority",
-        joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
-        inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")})
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "authority_name", referencedColumnName = "name"))
     @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
     @BatchSize(size = 20)
     private Set<Authority> authorities = new HashSet<>();
@@ -103,8 +103,8 @@ public class User extends AbstractAuditingEntity implements Serializable {
     @ManyToMany
     @JoinTable(
         name = "user_product",
-        joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
-        inverseJoinColumns = {@JoinColumn(name = "product_id", referencedColumnName = "id")}
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "product_id", referencedColumnName = "id")
     )
     private Set<ProductAccount> authorizedProducts = new HashSet<>();
 
--- src/main/java/ai/techfin/tradesystem/web/rest/ProductAccountResource.java
+++ src/main/java/ai/techfin/tradesystem/web/rest/ProductAccountResource.java
@@ -2,26 +2,22 @@ package ai.techfin.tradesystem.web.rest;
 
 import ai.techfin.tradesystem.domain.ProductAccount;
 import ai.techfin.tradesystem.repository.ProductAccountRepository;
+import ai.techfin.tradesystem.security.AuthoritiesConstants;
 import ai.techfin.tradesystem.web.rest.errors.BadRequestAlertException;
-
 import io.github.jhipster.web.util.HeaderUtil;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
-
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import static org.elasticsearch.index.query.QueryBuilders.*;
 
 /**
  * REST controller for managing {@link ai.techfin.tradesystem.domain.ProductAccount}.
@@ -51,6 +47,7 @@ public class ProductAccountResource {
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
     @PostMapping("/product-accounts")
+    @Secured(AuthoritiesConstants.ADMIN)
     public ResponseEntity<ProductAccount> createProductAccount(@Valid @RequestBody ProductAccount productAccount) throws URISyntaxException {
         log.debug("REST request to save ProductAccount : {}", productAccount);
         if (productAccount.getId() != null) {
@@ -63,45 +60,13 @@ public class ProductAccountResource {
     }
 
     /**
-     * {@code PUT  /product-accounts} : Updates an existing productAccount.
-     *
-     * @param productAccount the productAccount to update.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated productAccount,
-     * or with status {@code 400 (Bad Request)} if the productAccount is not valid,
-     * or with status {@code 500 (Internal Server Error)} if the productAccount couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PutMapping("/product-accounts")
-    public ResponseEntity<ProductAccount> updateProductAccount(@Valid @RequestBody ProductAccount productAccount) throws URISyntaxException {
-        log.debug("REST request to update ProductAccount : {}", productAccount);
-        if (productAccount.getId() == null) {
-            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-        }
-        ProductAccount result = productAccountRepository.save(productAccount);
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, productAccount.getId().toString()))
-            .body(result);
-    }
-
-    /**
-     * {@code GET  /product-accounts} : get all the productAccounts.
-     *
-
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of productAccounts in body.
-     */
-    @GetMapping("/product-accounts")
-    public List<ProductAccount> getAllProductAccounts() {
-        log.debug("REST request to get all ProductAccounts");
-        return productAccountRepository.findAll();
-    }
-
-    /**
      * {@code GET  /product-accounts/:id} : get the "id" productAccount.
      *
      * @param id the id of the productAccount to retrieve.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the productAccount, or with status {@code 404 (Not Found)}.
      */
     @GetMapping("/product-accounts/{id}")
+    @Secured({AuthoritiesConstants.ADMIN, AuthoritiesConstants.MODEL})
     public ResponseEntity<ProductAccount> getProductAccount(@PathVariable Long id) {
         log.debug("REST request to get ProductAccount : {}", id);
         Optional<ProductAccount> productAccount = productAccountRepository.findById(id);
@@ -109,16 +74,15 @@ public class ProductAccountResource {
     }
 
     /**
-     * {@code DELETE  /product-accounts/:id} : delete the "id" productAccount.
+     * {@code GET  /product-accounts} : get all the productAccounts.
      *
-     * @param id the id of the productAccount to delete.
-     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of productAccounts in body.
      */
-    @DeleteMapping("/product-accounts/{id}")
-    public ResponseEntity<Void> deleteProductAccount(@PathVariable Long id) {
-        log.debug("REST request to delete ProductAccount : {}", id);
-        productAccountRepository.deleteById(id);
-        return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
+    @GetMapping("/product-accounts")
+    @Secured({AuthoritiesConstants.ADMIN, AuthoritiesConstants.MODEL})
+    public List<ProductAccount> getAllProductAccounts() {
+        log.debug("REST request to get all ProductAccounts");
+        return productAccountRepository.findAll();
     }
 
 }
