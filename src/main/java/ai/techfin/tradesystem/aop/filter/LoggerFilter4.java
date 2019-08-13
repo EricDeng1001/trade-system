3781843e500a173de5e6bc1e097b0ae7d7b52106
GitAntinus <gantinus@gmail.com>
2019-08-14 01:07:38 +0800

    增加一组tracker Filter 便于Debug
    Signed-off-by: GitAntinus <gantinus@gmail.com>

---
+++ src/main/java/ai/techfin/tradesystem/aop/filter/LoggerFilter4.java
@@ -0,0 +1,24 @@
+package ai.techfin.tradesystem.aop.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoggerFilter4 extends OncePerRequestFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggerFilter4.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+        logger.debug("we reached on 4!");
+        filterChain.doFilter(request, response);
+    }
+
+}
