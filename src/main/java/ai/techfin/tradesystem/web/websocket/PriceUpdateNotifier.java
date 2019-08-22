95b97168d3b6aa622d7d44980591f58e5350a293
GitAntinus <gantinus@gmail.com>
2019-08-22 16:04:52 +0800

    搭建好内部价格信息流和外部价格信息流的桥梁
    Signed-off-by: GitAntinus <gantinus@gmail.com>

---
+++ src/main/java/ai/techfin/tradesystem/web/websocket/PriceUpdateNotifier.java
@@ -0,0 +1,24 @@
+package ai.techfin.tradesystem.web.websocket;
+
+import ai.techfin.tradesystem.domain.Stock;
+import ai.techfin.tradesystem.domain.enums.MarketType;
+import ai.techfin.tradesystem.web.websocket.dto.PriceUpdateDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.math.BigDecimal;
+
+@Controller
+public class PriceUpdateNotifier {
+
+    private static final Logger log = LoggerFactory.getLogger(PriceUpdateNotifier.class);
+
+    @SendTo("/topic/price-change")
+    @KafkaListener(topics = "price-change", groupId = "trade-system")
+    public PriceUpdateDTO updatePrice() {
+        return new PriceUpdateDTO(new Stock("000001", MarketType.SZ), BigDecimal.valueOf(2));
+    }
+}
