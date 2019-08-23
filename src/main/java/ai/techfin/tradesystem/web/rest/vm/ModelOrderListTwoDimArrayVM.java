f6a1c7606a082852470a0e0c765c72d066415a1b
GitAntinus <gantinus@gmail.com>
2019-08-23 11:25:08 +0800

    对接新的模型输出格式
    Signed-off-by: GitAntinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/web/rest/vm/ModelOrderListTwoDimArrayVM.java
+++ src/main/java/ai/techfin/tradesystem/web/rest/vm/ModelOrderListTwoDimArrayVM.java
@@ -1,10 +1,26 @@
 package ai.techfin.tradesystem.web.rest.vm;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Arrays;
 
 public class ModelOrderListTwoDimArrayVM {
 
     private String model;
+
+    private String product;
+
+    private Data data;
+
+    public ModelOrderListTwoDimArrayVM() {}
+
+    @Override
+    public String toString() {
+        return "ModelOrderListTwoDimArrayVM{" +
+            "model: " + model +
+            "orders: " + data +
+            "}";
+    }
 
     public String getProduct() {
         return product;
@@ -14,12 +30,6 @@ public class ModelOrderListTwoDimArrayVM {
         this.product = product;
     }
 
-    private String product;
-
-    private String[][] data;
-
-    public ModelOrderListTwoDimArrayVM() {}
-
     public String getModel() {
         return model;
     }
@@ -28,20 +38,38 @@ public class ModelOrderListTwoDimArrayVM {
         this.model = model;
     }
 
-    public String[][] getData() {
+    public Data getData() {
         return data;
     }
 
-    public void setData(String[][] data) {
+    public void setData(Data data) {
         this.data = data;
     }
 
-    @Override
-    public String toString() {
-        return "ModelOrderListTwoDimArrayVM{" +
-            "model: " + model +
-            "orders: " + Arrays.deepToString(data) +
-            "}";
+    private static class Data {
+
+        @JsonProperty("sell_list")
+        private String[][] sellList;
+
+        @JsonProperty("buy_list")
+        private String[][] buyList;
+
+        public String[][] getSellList() { return sellList; }
+
+        public void setSellList(String[][] sellList) { this.sellList = sellList; }
+
+        public String[][] getBuyList() { return buyList; }
+
+        public void setBuyList(String[][] buyList) { this.buyList = buyList; }
+
+        @Override
+        public String toString() {
+            return "{" +
+                Arrays.deepToString(sellList) +
+                Arrays.deepToString(buyList) +
+                "}";
+        }
+
     }
 
 }
