9652b8c3f61d0c71dccc75aadd21150893f87e8d
GitAntinus <gantinus@gmail.com>
2019-08-29 19:12:15 +0800

    进一步实现交易逻辑
    Signed-off-by: GitAntinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/service/ModelOrderService.java
+++ src/main/java/ai/techfin/tradesystem/service/ModelOrderService.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 @Service
 public class ModelOrderService {
@@ -15,9 +16,17 @@ public class ModelOrderService {
 
     public ModelOrderDTO loadDTOWithNewestPrice(ModelOrder modelOrder, BigDecimal totalAsset) {
         // TODO: 从消息中心获取价格
-        BigDecimal price = BigDecimal.valueOf(2);
-        BigDecimal money = totalAsset.multiply(modelOrder.getWeight());
-        return new ModelOrderDTO(modelOrder.getStock(), modelOrder.getMarket(), price, money);
+        final BigDecimal price = BigDecimal.valueOf(2);
+        final BigDecimal weight = modelOrder.getWeight();
+        BigDecimal money;
+        if (weight.compareTo(BigDecimal.ZERO) > 0) {
+            money = totalAsset.multiply(weight);
+        } else {
+            // TODO: 取得股票数量
+            final BigInteger quantity = BigInteger.valueOf(1000);
+            money = price.multiply(new BigDecimal(quantity));
+        }
+        return new ModelOrderDTO(modelOrder.getStock(), price, money);
     }
 
 }
