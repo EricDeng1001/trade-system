28c7e802d369ba428c84f3797c9f5b5e3fce4f2d
GitAntinus <gantinus@gmail.com>
2019-08-29 18:49:51 +0800

    改用SIMP发送消息
    Signed-off-by: GitAntinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/web/websocket/PriceUpdateNotifier.java
+++ src/main/java/ai/techfin/tradesystem/web/websocket/PriceUpdateNotifier.java
@@ -1,26 +1,28 @@
 package ai.techfin.tradesystem.web.websocket;
 
 import ai.techfin.tradesystem.config.KafkaTopicConfiguration;
-import ai.techfin.tradesystem.domain.Stock;
-import ai.techfin.tradesystem.domain.enums.MarketType;
-import ai.techfin.tradesystem.web.websocket.dto.PriceUpdateDTO;
+import ai.techfin.tradesystem.service.dto.PriceUpdateDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.math.BigDecimal;
 
 @Controller
 public class PriceUpdateNotifier {
 
     private static final Logger log = LoggerFactory.getLogger(PriceUpdateNotifier.class);
 
-    @SendTo("/topic/price-change")
-    @KafkaListener(topics = {KafkaTopicConfiguration.XTP_PRICE_CHANGE_TOPIC}, groupId = "trade-system")
-    public PriceUpdateDTO updatePrice(PriceUpdateDTO data) {
-        return data;
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    public PriceUpdateNotifier(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
     }
 
+    @KafkaListener(topics = KafkaTopicConfiguration.XTP_PRICE_CHANGE_TOPIC, id = "ws")
+    public void updatePrice(String data) {
+        log.info("going to push price-change message to user");
+        simpMessagingTemplate.convertAndSend("/topic/price-change", PriceUpdateDTO.fromString(data));
+    }
 }
