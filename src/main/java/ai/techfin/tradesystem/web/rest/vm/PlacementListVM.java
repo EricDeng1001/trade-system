95085fedfc2c393be7a7282ea8cd61bb0db45fdc
GitAntinus <gantinus@gmail.com>
2019-09-03 17:04:03 +0800

    增加网络输入层校验
    Signed-off-by: GitAntinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/web/rest/vm/PlacementListVM.java
+++ src/main/java/ai/techfin/tradesystem/web/rest/vm/PlacementListVM.java
@@ -4,14 +4,18 @@ import ai.techfin.tradesystem.domain.Placement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 public class PlacementListVM {
 
     private static final Logger log = LoggerFactory.getLogger(PlacementListVM.class);
 
+    @NotNull
     private Long modelOrderListId;
 
+    @NotNull
     private Set<Placement> placements;
 
     public Long getModelOrderListId() { return modelOrderListId; }
