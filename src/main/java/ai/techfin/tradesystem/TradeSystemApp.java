6e1947d9c8365c5ad674945bc78e066dc8ae9de8
GitAntinus <gantinus@gmail.com>
2019-09-03 17:05:23 +0800

    better naming
    Signed-off-by: GitAntinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/TradeSystemApp.java
+++ src/main/java/ai/techfin/tradesystem/TradeSystemApp.java
@@ -21,14 +21,14 @@ import java.util.Collection;
 @EnableConfigurationProperties({ApplicationProperties.class})
 public class TradeSystemApp implements InitializingBean {
 
-    private static final Logger logger = LoggerFactory.getLogger(TradeSystemApp.class);
+    private static final Logger log = LoggerFactory.getLogger(TradeSystemApp.class);
 
     private final Environment env;
 
     @Autowired
     public TradeSystemApp(Environment env, ApplicationProperties applicationProperties) {
         this.env = env;
-        logger.info("written by: {}", applicationProperties.getAuthor());
+        log.info("written by: {}", applicationProperties.getAuthor());
     }
 
     /**
@@ -52,7 +52,7 @@ public class TradeSystemApp implements InitializingBean {
         Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
         if (activeProfiles.contains(ApplicationConstants.Profile.DEVELOPMENT) &&
             activeProfiles.contains(ApplicationConstants.Profile.PRODUCTION)) {
-            logger.error(
+            log.error(
                 "Application should not run with both the 'dev' and 'prod' profiles at the same time."
             );
             throw new Error("Application should not run with both the 'dev' and 'prod' profiles at the same time.");
@@ -73,22 +73,22 @@ public class TradeSystemApp implements InitializingBean {
         try {
             hostAddress = InetAddress.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
-            logger.warn("The host name could not be determined, using `localhost` as fallback");
+            log.warn("The host name could not be determined, using `localhost` as fallback");
         }
-        logger.info("\n----------------------------------------------------------\n\t" +
+        log.info("\n----------------------------------------------------------\n\t" +
                         "Application '{}' is running! Access URLs:\n\t" +
                         "Local: \t\t{}://localhost:{}{}\n\t" +
                         "External: \t{}://{}:{}{}\n\t" +
                         "Profile(s): \t{}\n----------------------------------------------------------",
-                    env.getProperty("spring.application.name"),
-                    protocol,
-                    serverPort,
-                    contextPath,
-                    protocol,
-                    hostAddress,
-                    serverPort,
-                    contextPath,
-                    env.getActiveProfiles());
+                 env.getProperty("spring.application.name"),
+                 protocol,
+                 serverPort,
+                 contextPath,
+                 protocol,
+                 hostAddress,
+                 serverPort,
+                 contextPath,
+                 env.getActiveProfiles());
     }
 
 }
