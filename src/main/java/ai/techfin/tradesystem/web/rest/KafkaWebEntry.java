bce5a614b732bae23d15e4f347fb993b9bb118c5
GitAntinus <gantinus@gmail.com>
2019-09-03 17:05:01 +0800

    设置仅有管理员可以手动推送消息到交易系统
    Signed-off-by: GitAntinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/web/rest/KafkaWebEntry.java
+++ src/main/java/ai/techfin/tradesystem/web/rest/KafkaWebEntry.java
@@ -1,9 +1,14 @@
 package ai.techfin.tradesystem.web.rest;
 
+import ai.techfin.tradesystem.security.AuthoritiesConstants;
 import ai.techfin.tradesystem.service.KafkaEventProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/trade-system-kafka")
@@ -18,8 +23,10 @@ public class KafkaWebEntry {
     }
 
     @PostMapping(value = "/publish")
+    @Secured(AuthoritiesConstants.ADMIN)
     public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
         log.debug("REST request to send to Kafka topic the message : {}", message);
         this.kafkaProducer.sendMessage(message);
     }
+
 }
