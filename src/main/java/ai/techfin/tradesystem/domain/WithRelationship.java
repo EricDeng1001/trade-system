1dfb06223bd28fcb53d7bbef23f2c177134c2bae
GitAntinus <gantinus@gmail.com>
2019-09-05 18:55:41 +0800

    占坑，日后继续研究
    Signed-off-by: GitAntinus <gantinus@gmail.com>

---
+++ src/main/java/ai/techfin/tradesystem/domain/WithRelationship.java
@@ -0,0 +1,10 @@
+package ai.techfin.tradesystem.domain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+abstract class WithRelationship<T, R> {
+
+    private static final Logger log = LoggerFactory.getLogger(WithRelationship.class);
+
+}
