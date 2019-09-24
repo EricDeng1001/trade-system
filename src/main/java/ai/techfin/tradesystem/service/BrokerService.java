e43e45ea6a3a896bb6a5823c903b858f0500c919
gantinus <gantinus@gmail.com>
2019-09-24 17:15:30 +0800

    修改接口设计
    Signed-off-by: gantinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/service/BrokerService.java
+++ src/main/java/ai/techfin/tradesystem/service/BrokerService.java
@@ -2,14 +2,18 @@ package ai.techfin.tradesystem.service;
 
 import ai.techfin.tradesystem.domain.Stock;
 import ai.techfin.tradesystem.domain.enums.PriceType;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Map;
 
+@Service
 public interface BrokerService {
 
-    boolean buy(String user, Stock stock, double price, PriceType priceType);
+    boolean buy(String user, Stock stock, BigInteger quantity, BigDecimal price, PriceType priceType);
 
-    boolean sell(String user, Stock stock, double price, PriceType priceType);
+    boolean sell(String user, Stock stock, BigInteger quantity, BigDecimal price, PriceType priceType);
 
     boolean init();
 
