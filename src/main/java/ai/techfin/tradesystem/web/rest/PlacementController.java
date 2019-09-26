38b616126b2b1d9fe73312571fd934a196b06a03
gantinus <gantinus@gmail.com>
2019-09-26 12:03:34 +0800

    修复bug
    Signed-off-by: gantinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/web/rest/PlacementController.java
+++ src/main/java/ai/techfin/tradesystem/web/rest/PlacementController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class PlacementController {
 
