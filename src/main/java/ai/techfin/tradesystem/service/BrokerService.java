5cedb7a97fb8366e924e2711ca14ee5994db88b0
gantinus <gantinus@gmail.com>
2019-10-03 00:37:38 +0800

    优化代码
    Signed-off-by: gantinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/service/BrokerService.java
+++ src/main/java/ai/techfin/tradesystem/service/BrokerService.java
@@ -2,7 +2,6 @@ package ai.techfin.tradesystem.service;
 
 import ai.techfin.tradesystem.domain.Stock;
 import ai.techfin.tradesystem.domain.enums.PriceType;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
