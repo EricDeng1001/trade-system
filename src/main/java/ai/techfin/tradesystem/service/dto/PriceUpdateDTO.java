f3090b042274ed058f20925ba30730a8abe07a3f
gantinus <gantinus@gmail.com>
2019-10-03 00:33:02 +0800

    由原来的String表示转换为枚举类型表示
    Signed-off-by: gantinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/service/dto/PriceUpdateDTO.java
+++ src/main/java/ai/techfin/tradesystem/service/dto/PriceUpdateDTO.java
@@ -1,7 +1,7 @@
 package ai.techfin.tradesystem.service.dto;
 
 import ai.techfin.tradesystem.domain.Stock;
-import ai.techfin.tradesystem.domain.enums.MarketType;
+import ai.techfin.tradesystem.domain.enums.BrokerType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,36 +18,24 @@ public class PriceUpdateDTO implements Serializable {
 
     private BigDecimal price;
 
-    private String broker;
+    private BrokerType broker;
 
-    public PriceUpdateDTO(Stock stock, BigDecimal price, String broker) {
+    public PriceUpdateDTO(Stock stock, BigDecimal price, BrokerType broker) {
         this.stock = stock;
         this.price = price;
         this.broker = broker;
     }
 
-    public static PriceUpdateDTO fromString(String value) {
-        return new PriceUpdateDTO(new Stock("000001", MarketType.SZ),
-                                  BigDecimal.valueOf(20 + Math.random() * 10),
-                                  "xtp"
-        );
+    public Stock getStock() {
+        return stock;
     }
 
-    @Override
-    public String toString() {
-        return stock + "," + price + "," + broker;
+    public BigDecimal getPrice() {
+        return price;
     }
 
-    public Stock getStock() { return stock; }
-
-    public void setStock(Stock stock) { this.stock = stock; }
-
-    public String getBroker() { return broker; }
-
-    public void setBroker(String broker) { this.broker = broker; }
-
-    public BigDecimal getPrice() { return price; }
-
-    public void setPrice(BigDecimal price) { this.price = price; }
+    public BrokerType getBroker() {
+        return broker;
+    }
 
 }
