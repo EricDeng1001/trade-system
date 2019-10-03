46c3e29e142698b56295eed5540ff090ac6d7585
gantinus <gantinus@gmail.com>
2019-10-04 00:49:48 +0800

    增加InternalBroker对象设计，用于内部测试等。
    Signed-off-by: gantinus <gantinus@gmail.com>

---
+++ src/main/java/ai/techfin/tradesystem/service/impl/InternalBroker.java
@@ -0,0 +1,43 @@
+package ai.techfin.tradesystem.service.impl;
+
+import ai.techfin.tradesystem.config.ApplicationConstants;
+import ai.techfin.tradesystem.domain.Stock;
+import ai.techfin.tradesystem.domain.enums.PriceType;
+import ai.techfin.tradesystem.service.BrokerService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Map;
+
+@Service(ApplicationConstants.BrokerService.INTERNAL)
+public class InternalBroker implements BrokerService {
+
+    @Override
+    public boolean buy(String user, Long placementId, Stock stock, BigInteger quantity, BigDecimal price,
+                       PriceType priceType) {
+        return false;
+    }
+
+    @Override
+    public boolean sell(String user, Long placementId, Stock stock, BigInteger quantity, BigDecimal price,
+                        PriceType priceType) {
+        return false;
+    }
+
+    @Override
+    public boolean init() {
+        return false;
+    }
+
+    @Override
+    public boolean loginUser(String user, String password, Map<String, String> additional) {
+        return false;
+    }
+
+    @Override
+    public boolean subscribePrice(Stock stock) {
+        return false;
+    }
+
+}
