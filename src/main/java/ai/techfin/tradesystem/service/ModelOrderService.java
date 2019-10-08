8840092661ac6d2adb3bc8c3da108948945410ba
gantinus <gantinus@gmail.com>
2019-10-09 00:13:29 +0800

    add prepareTrade method
    Signed-off-by: gantinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/tradesystem/service/ModelOrderService.java
+++ src/main/java/ai/techfin/tradesystem/service/ModelOrderService.java
@@ -1,14 +1,14 @@
 package ai.techfin.tradesystem.service;
 
 import ai.techfin.tradesystem.domain.ModelOrder;
+import ai.techfin.tradesystem.domain.ModelOrderList;
+import ai.techfin.tradesystem.domain.Product;
 import ai.techfin.tradesystem.domain.enums.BrokerType;
 import ai.techfin.tradesystem.service.dto.ModelOrderDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
 
 @Service
 public class ModelOrderService {
@@ -17,11 +17,23 @@ public class ModelOrderService {
 
     private final PriceService priceService;
 
+    private final TradeService tradeService;
+
     @Autowired
-    public ModelOrderService(PriceService priceService) {this.priceService = priceService;}
+    public ModelOrderService(PriceService priceService, TradeService tradeService) {
+        this.priceService = priceService;
+        this.tradeService = tradeService;
+    }
 
     public ModelOrderDTO createDTO(ModelOrder modelOrder, BrokerType brokerType) {
         return new ModelOrderDTO(modelOrder, priceService.getPrice(modelOrder.getStock(), brokerType));
+    }
+
+    public void prepareTrade(ModelOrderList modelOrderList) {
+        Product product = modelOrderList.getProduct();
+        tradeService.loginProductAccount(product);
+        modelOrderList.getOrders()
+            .forEach(o -> tradeService.subscribeStockPrice(o.getStock(), product.getProvider()));
     }
 
 }
