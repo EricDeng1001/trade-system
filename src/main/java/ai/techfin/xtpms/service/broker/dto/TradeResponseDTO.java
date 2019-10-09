85ffc7d78e0279b385af49119e770232b3db8248
gantinus <gantinus@gmail.com>
2019-10-09 11:55:05 +0800

    TradeResponse定义
    Signed-off-by: gantinus <gantinus@gmail.com>

--- src/main/java/ai/techfin/xtpms/service/broker/dto/TradeResponseDTO.java
+++ src/main/java/ai/techfin/xtpms/service/broker/dto/TradeResponseDTO.java
@@ -1,106 +1,27 @@
 package ai.techfin.xtpms.service.broker.dto;
 
-import ai.techfin.tradesystem.domain.enums.MarketType;
-import ai.techfin.tradesystem.domain.enums.SideType;
+import ai.techfin.tradesystem.domain.Stock;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
+@ToString
+@AllArgsConstructor
 public class TradeResponseDTO implements Serializable {
 
-    private String placementId;
+    @Getter
+    private Long placementId;
 
-    private String tradeId;
+    @Getter
+    private Long quantity;
 
-    private long quantity;
+    @Getter
+    private BigDecimal price;
 
-    private double price;
+    @Getter
+    private Stock stock;
 
-    private String user;
-
-    private String ticker;
-
-    private MarketType marketType;
-
-    private SideType sideType;
-
-    public String getPlacementId() {
-        return placementId;
-    }
-
-    public void setPlacementId(String placementId) {
-        this.placementId = placementId;
-    }
-
-    public String getTradeId() {
-        return tradeId;
-    }
-
-    public void setTradeId(String tradeId) {
-        this.tradeId = tradeId;
-    }
-
-    public long getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(long quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getTicker() {
-        return ticker;
-    }
-
-    public void setTicker(String ticker) {
-        this.ticker = ticker;
-    }
-
-    public MarketType getMarketType() {
-        return marketType;
-    }
-
-    public void setMarketType(MarketType marketType) {
-        this.marketType = marketType;
-    }
-
-    public SideType getSideType() {
-        return sideType;
-    }
-
-    public void setSideType(SideType sideType) {
-        this.sideType = sideType;
-    }
-
-    public TradeResponseDTO() {
-    }
-
-    @Override
-    public String toString() {
-        return "TradeResponseDTO{" +
-                "placementId='" + placementId + '\'' +
-                ", tradeId='" + tradeId + '\'' +
-                ", quantity=" + quantity +
-                ", price=" + price +
-                ", user='" + user + '\'' +
-                ", ticker='" + ticker + '\'' +
-                ", marketType=" + marketType +
-                ", sideType=" + sideType +
-                '}';
-    }
 }
